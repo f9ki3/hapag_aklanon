173325a4173f1b743e01e0b7dff61c229dc51f89
Fyke Lleva <floterina@gmail.com>
2026-02-07 23:09:56 +0800

    updated

--- app.py
+++ app.py
@@ -454,5 +454,6 @@ def internal_error(error):
     return jsonify({"success": False, "error": "Internal server error"}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    
+    app.run(host='0.0.0.0')
 
